eb43357f3d2e7672cf1aafcc94a2f6628ab68f3d
Regina Buckenmyer <gina.buckenmyer@yahoo.com>
2017-01-26 18:56:59 -0600

    Fixed error

--- main.py
+++ main.py
@@ -15,19 +15,19 @@
 # limitations under the License.
 #
 import webapp2
-import cgi
 import caesar
+import cgi
 
 def build_page(textarea_content):
     rot_label="<label>Rotate by:</label>"
     rotation_input = "<input type='number' name='rotation'/>"
-    message_label = "<label>"Type a message: "</label>"
+    message_label = "<label>Type a message: </label>"
     textarea = "<textarea name='message'>" + textarea_content + "</textarea>"
 
     submit = "<input type='submit'/>"
 
-    form = "<form method='post'>"+ rot_label+
-    rotation_input + "<br>" + textarea + "<br>" + submit+ "</form>"
+    form = ("<form method='post'>"+ rot_label+
+    rotation_input + "<br>" + textarea + "<br>" + submit+ "</form>")
 
     header = "<h2>Web Caesar</h2>"
     return header + form
